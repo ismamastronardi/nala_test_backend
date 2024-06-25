1b428247e13053fa993b9549b4c2d975af4d7a3f
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-06-25 20:34:17 -0300

    add days_absent method to employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,4 +3,9 @@ class Employee < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
+
+  def days_absent
+    absence_requests.where.not(status: "rejected").sum(&:days_requested)
+  end
+
 end
