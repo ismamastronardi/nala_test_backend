8957e6b5d3b252119ff5ed979da0ea675211e1a0
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-06-25 20:33:52 -0300

    add days_requested method to absende_request model

--- app/models/absence_request.rb
+++ app/models/absence_request.rb
@@ -5,4 +5,8 @@ class AbsenceRequest < ApplicationRecord
   validates :end_date, presence: true
   validates :request_type, presence: true
   validates :status, presence: true
+
+  def days_requested
+    (end_date - start_date).to_i
+  end
 end
