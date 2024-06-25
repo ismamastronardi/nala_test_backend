7d3be5afec8cdc321c54c2afbe5f6797259144ac
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-06-25 18:53:04 -0300

    add employee and absence_request model validations and relations

--- app/models/absence_request.rb
+++ app/models/absence_request.rb
@@ -1,2 +1,8 @@
 class AbsenceRequest < ApplicationRecord
+  belongs_to :employee
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :request_type, presence: true
+  validates :status, presence: true
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ApplicationRecord
+  has_many :absence_requests
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
