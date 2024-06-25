cea85f81764dc6ce1ce130e44c0551a049372a1e
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-06-25 20:08:49 -0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :absence_requests
   resources :employees do
     resources :absence_requests
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :absence_requests, only: [:index, :show, :create, :update, :destroy]
 end
