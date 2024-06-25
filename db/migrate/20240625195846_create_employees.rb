75a3736fe0816f6afd74b145cec603372810392e
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-06-25 17:02:10 -0300

    add CreateEmployees migration file

---
+++ db/migrate/20240625195846_create_employees.rb
@@ -0,0 +1,10 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.string :name, limit: 255, null: false
+      t.string :email, limit: 255, null: false, unique: true
+
+      t.timestamps
+    end
+  end
+end
