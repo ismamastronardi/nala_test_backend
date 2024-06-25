df82a2fb2e1848ef59495dd4b01c7ee1fc680b0b
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-06-25 17:21:29 -0300

    add employees related files

--- db/migrate/20240625200911_create_employees.rb
+++ db/migrate/20240625200911_create_employees.rb
@@ -2,7 +2,7 @@ class CreateEmployees < ActiveRecord::Migration[6.1]
   def change
     create_table :employees do |t|
       t.string :name, limit: 255, null: false
-      t.string :email, limit: 255, null: false, unique: true
+      t.string :email, limit: 255, null: false
 
       t.timestamps
     end
