7f971ea802b0b0165b51f9463b24121490458a8e
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-06-26 18:53:37 -0300

    add start_date and end_date filters

--- app/controllers/absence_requests_controller.rb
+++ app/controllers/absence_requests_controller.rb
@@ -17,6 +17,12 @@ class AbsenceRequestsController < ApplicationController
     if params[:status].present?
       absence_requests = absence_requests.where(status: params[:status])
     end
+    if params[:start_date].present?
+      absence_requests = absence_requests.where("start_date >= ?", params[:start_date])
+    end
+    if params[:end_date].present?
+      absence_requests = absence_requests.where("start_date <= ?", params[:end_date])
+    end
     @pagy, paginated_absence_requests = pagy(absence_requests, items: 10)
     render json: {absence_request: paginated_absence_requests, pagy: { pages: @pagy.pages, current_page: @pagy.page }}
   end
